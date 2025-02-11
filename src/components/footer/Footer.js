ebf5d5b09478f06dd72da533411cff1ce7a23f95
mazolo <right.mazolo@nbs.mw>
2025-02-11 23:24:10 +0200

    enabled the projrct component

--- src/components/footer/Footer.js
+++ src/components/footer/Footer.js
@@ -30,12 +30,12 @@ const Footer = () => {
               <span className="w-full h-[1px] bg-designColor inline-flex absolute left-0 -bottom-1 -translate-x-[100%] group-hover:translate-x-0 transition-transform duration-300"></span>
             </span>
           </li>
-          <li>
+          {/* <li>
             <span className="w-full text-lg relative hover:text-designColor duration-300 group cursor-pointer">
               Portfolio
               <span className="w-full h-[1px] bg-designColor inline-flex absolute left-0 -bottom-1 -translate-x-[100%] group-hover:translate-x-0 transition-transform duration-300"></span>
             </span>
-          </li>
+          </li> */}
           <li>
             <span className="w-full text-lg relative hover:text-designColor duration-300 group cursor-pointer">
               Services
@@ -61,7 +61,7 @@ const Footer = () => {
           RESOURCES
         </h3>
         <ul className="flex flex-col gap-4 font-titleFont font-medium py-6 overflow-hidden">
-          <li>
+          {/* <li>
             <span className="w-full text-lg relative hover:text-designColor duration-300 group cursor-pointer">
               Authentication
               <span className="w-full h-[1px] bg-designColor inline-flex absolute left-0 -bottom-1 -translate-x-[100%] group-hover:translate-x-0 transition-transform duration-300"></span>
@@ -90,15 +90,15 @@ const Footer = () => {
               Over Right
               <span className="w-full h-[1px] bg-designColor inline-flex absolute left-0 -bottom-1 -translate-x-[100%] group-hover:translate-x-0 transition-transform duration-300"></span>
             </span>
-          </li>
+          </li> */}
         </ul>
       </div>
       <div className="w-full h-full">
         <h3 className="text-xl uppercase text-designColor tracking-wider">
-          DEVELOPERS
+          Other
         </h3>
         <ul className="flex flex-col gap-4 font-titleFont font-medium overflow-hidden py-6">
-          <li>
+          {/* <li>
             <span className="w-full text-lg relative hover:text-designColor duration-300 group cursor-pointer">
               Documentation
               <span className="w-full h-[1px] bg-designColor inline-flex absolute left-0 -bottom-1 -translate-x-[100%] group-hover:translate-x-0 transition-transform duration-300"></span>
@@ -127,7 +127,7 @@ const Footer = () => {
               Open Source
               <span className="w-full h-[1px] bg-designColor inline-flex absolute left-0 -bottom-1 -translate-x-[100%] group-hover:translate-x-0 transition-transform duration-300"></span>
             </span>
-          </li>
+          </li> */}
         </ul>
       </div>
     </div>
